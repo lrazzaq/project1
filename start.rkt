b88551115137af4e5d80b62ad968c935b7944fdb
xeniamount <xmountr@ju.edu>
2014-03-05 11:56:21 -0500

    Update start.rkt

--- start.rkt
+++ start.rkt
@@ -1,5 +1,6 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
+;; I have no idea about racket but this program looks cool!
 #reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname start) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
 (require 2htdp/image)
 
